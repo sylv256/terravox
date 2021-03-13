fddccf7e10c738b95c3d723dd07dbb202d8f5c44
TehcJS <tehcdev@gmail.com>
2021-03-13 15:40:41 -0500

    Update to OpenGL 4.5 (i'm not supporting stupid mac)

--- src/build.rs
+++ src/build.rs
@@ -9,7 +9,7 @@ fn main() {
 	let ref dest = env::var("OUT_DIR").unwrap();
 	let ref mut file = File::create(Path::new(dest).join("bindings.rs")).unwrap();
 
-	Registry::new(Api::Gl, (4, 1), Profile::Core, Fallbacks::All, [])
+	Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, [])
 		.write_bindings(StructGenerator, file)
 		.unwrap();
 }
